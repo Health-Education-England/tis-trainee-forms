332990e9dd238eaf88f5e28f308880f9462e8a5e
Doris Wong <doris.wong@hee.nhs.uk>
2020-05-04 13:15:07 +0100

    feat: add Form R Part B
    TISNEW-4283

---
+++ src/main/java/uk/nhs/hee/tis/trainee/forms/api/FormRPartBResource.java
@@ -0,0 +1,88 @@
+/*
+ * The MIT License (MIT)
+ * Copyright 2020 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.forms.api;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.nhs.hee.tis.trainee.forms.api.util.HeaderUtil;
+import uk.nhs.hee.tis.trainee.forms.dto.FormRPartBDto;
+import uk.nhs.hee.tis.trainee.forms.service.FormRPartBService;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class FormRPartBResource {
+
+  private static final String ENTITY_NAME = "formR_partB";
+
+  private FormRPartBService formRPartBService;
+
+  public FormRPartBResource(FormRPartBService formRPartBService) {
+    this.formRPartBService = formRPartBService;
+  }
+
+  /**
+   * POST  /formr-partb : Create a new FormRPartB.
+   *
+   * @param formRPartBDto the formRPartBDto to create
+   * @return the ResponseEntity with status 201 (Created) and with body the new formRPartBDto, or
+   * with status 400 (Bad Request) if the formRPartB has already an ID
+   * @throws URISyntaxException if the Location URI syntax is incorrect
+   */
+  @PostMapping("/formr-partb")
+  public ResponseEntity<FormRPartBDto> createFormRPartB(
+      @RequestBody FormRPartBDto formRPartBDto) throws URISyntaxException {
+    log.debug("REST request to save FormRPartB : {}", formRPartBDto);
+    if (formRPartBDto.getId() != null) {
+      return ResponseEntity.badRequest().headers(HeaderUtil
+          .createFailureAlert(ENTITY_NAME, "idexists",
+              "A new formRpartB cannot already have an ID"))
+          .body(null);
+    }
+    FormRPartBDto result = formRPartBService.save(formRPartBDto);
+    return ResponseEntity.created(new URI("/api/formr-partb/" + result.getId()))
+        .body(result);
+  }
+
+  /**
+   * GET /formr-partb/:traineeTisId.
+   *
+   * @param traineeProfileId
+   * @return list of formR partB based on the traineeTisId
+   */
+  @GetMapping("/formr-partb/{traineeTisId}")
+  public ResponseEntity<List<FormRPartBDto>> getFormRPartBsByTraineeId(
+      @PathVariable(name = "traineeTisId") String traineeProfileId) {
+    log.debug("FormR-PartB of a trainee by traineeTisId {}", traineeProfileId);
+    List<FormRPartBDto> formRPartBDtos = formRPartBService
+        .getFormRPartBsByTraineeTisId(traineeProfileId);
+    return ResponseEntity.ok(formRPartBDtos);
+  }
+}
---
+++ src/main/java/uk/nhs/hee/tis/trainee/forms/service/impl/FormRPartBServiceImpl.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License (MIT)
+ * Copyright 2020 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.forms.service.impl;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.nhs.hee.tis.trainee.forms.dto.FormRPartBDto;
+import uk.nhs.hee.tis.trainee.forms.mapper.FormRPartBMapper;
+import uk.nhs.hee.tis.trainee.forms.model.FormRPartB;
+import uk.nhs.hee.tis.trainee.forms.repository.FormRPartBRepository;
+import uk.nhs.hee.tis.trainee.forms.service.FormRPartBService;
+
+@Slf4j
+@Service
+@Transactional
+public class FormRPartBServiceImpl implements FormRPartBService {
+
+  private final FormRPartBMapper formRPartBMapper;
+
+  private final FormRPartBRepository formRPartBRepository;
+
+  public FormRPartBServiceImpl(FormRPartBRepository formRPartBRepository,
+      FormRPartBMapper formRPartBMapper) {
+    this.formRPartBRepository = formRPartBRepository;
+    this.formRPartBMapper = formRPartBMapper;
+  }
+
+  public FormRPartBDto save(FormRPartBDto formRPartBDto) {
+    log.info("Request to save FormRPartB : {}", formRPartBDto);
+    FormRPartB formRPartB = formRPartBMapper.toEntity(formRPartBDto);
+    formRPartB = formRPartBRepository.save(formRPartB);
+    return formRPartBMapper.toDto(formRPartB);
+  }
+
+  public List<FormRPartBDto> getFormRPartBsByTraineeTisId(String traineeTisId) {
+    log.info("Request to get FormRPartB list by trainee profileId : {}", traineeTisId);
+    List<FormRPartB> formRPartBList = formRPartBRepository.findByTraineeTisId(traineeTisId);
+    return formRPartBMapper.toDtos(formRPartBList);
+  }
+}
