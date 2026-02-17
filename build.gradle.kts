60e6a6517db025d2b6237f9e3de56aff915a649e
Reuben Roberts <reuben.roberts@nhs.net>
2026-02-17 15:37:03 +0000

    fix: testcontainers version
    Test PR

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,9 +109,9 @@ testing {
     register<JvmTestSuite>("integrationTest") {
       dependencies {
         implementation("org.springframework.boot:spring-boot-testcontainers")
-        implementation("org.testcontainers:junit-jupiter")
-        implementation("org.testcontainers:localstack")
-        implementation("org.testcontainers:mongodb")
+        implementation("org.testcontainers:junit-jupiter:1.19.7")
+        implementation("org.testcontainers:localstack:1.19.7")
+        implementation("org.testcontainers:mongodb:1.19.7")
         implementation("org.awaitility:awaitility")
       }
 
